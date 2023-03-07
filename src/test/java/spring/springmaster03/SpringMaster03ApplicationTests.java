29b38ef0f596c6d8474545c19dd4fa267221d3a5
prolkh <prolkh@gmail.com>
2023-03-07 23:40:38 +0900

    Spring MVC Part 1 - Core Technologies for Backend Web Development

---
+++ src/test/java/spring/springmaster03/SpringMaster03ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springmaster03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMaster03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
