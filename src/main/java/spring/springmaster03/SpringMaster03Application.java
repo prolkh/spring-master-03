b3dae5d92f561a057bb4087331f074d5f07fc215
prolkh <prolkh@gmail.com>
2023-03-08 23:55:57 +0900

    Hello servlet Invocation

--- src/main/java/spring/springmaster03/SpringMaster03Application.java
+++ src/main/java/spring/springmaster03/SpringMaster03Application.java
@@ -2,7 +2,9 @@ package spring.springmaster03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // Automatic Servlet Registration
 @SpringBootApplication
 public class SpringMaster03Application {
 
