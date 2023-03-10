9d8455fe3042d778b303596be8a6d0c3f705092d
prolkh <prolkh@gmail.com>
2023-03-10 23:42:50 +0900

    HttpServletRequest - GET query parameter

---
+++ src/main/java/spring/springmaster03/basic/request/RequestParamServlet.java
@@ -0,0 +1,42 @@
+package spring.springmaster03.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * 1. Parameter transmission function
+ * http://localhost:8080/request-param?username=hello&age=20&username=kim
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("[All Parameter Search] - Start");
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + " = " + request.getParameter(paramName)));
+        System.out.println("[All Parameter Search] - End");
+        System.out.println();
+
+        System.out.println("[Single Parameter Search]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println();
+
+        System.out.println("[Query for multiple parameters with the same name.]");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("name = " + name);
+        }
+
+        response.getWriter().write("ok");
+    }
+}
