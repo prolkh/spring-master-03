ec45621672620dd86b16f439a8405804300686b8
prolkh <prolkh@gmail.com>
2023-03-23 23:52:03 +0900

    v4 : Change to a simple and practical controller for ease of development

---
+++ src/main/java/spring/springmaster03/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -0,0 +1,24 @@
+package spring.springmaster03.web.frontcontroller.v4.controller;
+
+import spring.springmaster03.domain.member.Member;
+import spring.springmaster03.domain.member.MemberRepository;
+import spring.springmaster03.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberSaveControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.put("member", member);
+        return "save-result";
+    }
+}
