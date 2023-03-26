14b88004ad192c5133ea56b70b45803aa81a503c
prolkh <prolkh@gmail.com>
2023-03-26 23:43:13 +0900

    HandlerMapping and HandlerAdapter

---
+++ src/main/java/spring/springmaster03/web/springmvc/old/OldController.java
@@ -0,0 +1,21 @@
+package spring.springmaster03.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * HandlerMapping = BeanNameUrlHandlerMapping
+ * HandlerAdapter = SimpleControllerHandleAdapter
+ */
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
