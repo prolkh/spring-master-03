b734cc615b800a98fb7ed8d56954b689d1877937
prolkh <prolkh@gmail.com>
2023-03-27 23:56:01 +0900

    Spring MVC - Getting Started

---
+++ src/main/java/spring/springmaster03/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,26 @@
+package spring.springmaster03.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import spring.springmaster03.domain.member.Member;
+import spring.springmaster03.domain.member.MemberRepository;
+import spring.springmaster03.web.frontcontroller.ModelView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+
+        return mv;
+    }
+}
